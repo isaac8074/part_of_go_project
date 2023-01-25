67b049a856a8a460a9873c5ca53d373bdb54ec20
Isaac Bustamante <isaacbustamante99@gmail.com>
2023-01-25 16:46:45 -0600

    Next Commit

---
+++ ch4/ex4.13/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.19
+
+require github.com/metal3d/go-slugify v0.0.0-20160607203414-7ac2014b2f23
---
+++ ch5/findlinks2/main.go
@@ -0,0 +1,26 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 125.
+
+// Findlinks2 does an HTTP GET on each URL, parses the
+// result as HTML, and prints the links within it.
+//
+// Usage:
+//	findlinks url ...
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+// visit appends to links each link found in n, and returns the result.
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := 
+	}
+}
